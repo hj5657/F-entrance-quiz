5c1345893fe7e7355f633858fc7f9fd7459b4098
hejie <565703319@qq.com>
2020-10-27 19:27:28 +0800

    [hejie]: test front and backed

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
+  componentDidMount = () => {
+    fetch('http://localhost:8080/students', { method: 'GET', mode: 'no-cors' }).then((res) => {
+      console.log(res.json);
+    });
+  };
+
   render() {
     return (
       <div data-testid="app" className="App">
