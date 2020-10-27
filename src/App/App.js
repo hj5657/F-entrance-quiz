a49481fb70c5ac999b5c3fd3812de0f8790f015d
hejie <565703319@qq.com>
2020-10-27 21:37:28 +0800

    [hejie]: add init list

--- src/App/App.js
+++ src/App/App.js
@@ -2,16 +2,44 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allStudents: [],
+    };
+  }
+
   componentDidMount = () => {
-    fetch('http://localhost:8080/students', { method: 'GET', mode: 'no-cors' }).then((res) => {
-      console.log(res.json);
-    });
+    fetch('http://localhost:8080/students', { method: 'GET', mode: 'cors' })
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        this.setState({
+          allStudents: json,
+        });
+        console.log(json);
+      });
   };
 
   render() {
     return (
       <div data-testid="app" className="App">
-        Hello World
+        <div className="divide-list">
+          <h3>分组列表</h3>
+          <button type="button" className="divide-btn" name="divide-btn" value="分组学员">
+            分组学员
+          </button>
+        </div>
+        <div className="student-list">
+          <h3>学员列表</h3>
+          {this.state.allStudents.map((item) => {
+            return <div className="item">{`${item.id} ${item.name}`}</div>;
+          })}
+          <button type="button" className="add-btn" name="add-btn" value="+添加学员">
+            +添加学员
+          </button>
+        </div>
       </div>
     );
   }
