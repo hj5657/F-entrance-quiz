5bb346d3d255320e0d4278fb3a9b1fd91fb0b2cb
hejie <565703319@qq.com>
2020-10-27 22:29:54 +0800

    [hejie]: add divide

--- src/App/App.js
+++ src/App/App.js
@@ -6,6 +6,8 @@ class App extends Component {
     super(props);
     this.state = {
       allStudents: [],
+      studentList: [],
+      isShow: false,
     };
   }
 
@@ -18,11 +20,24 @@ class App extends Component {
         this.setState({
           allStudents: json,
         });
-        console.log(json);
+      });
+  };
+
+  handleDivide = () => {
+    fetch('http:/localhost:8080/studentList', { method: 'GET', mode: 'cors' })
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        this.setState({
+          studentList: json,
+          isShow: true,
+        });
       });
   };
 
   render() {
+    const { students, formatter } = this.state.studentList;
     return (
       <div data-testid="app" className="App">
         <div className="divide-list">
@@ -30,6 +45,19 @@ class App extends Component {
           <button type="button" className="divide-btn" name="divide-btn" value="分组学员">
             分组学员
           </button>
+          {this.state.isShow &&
+            formatter.map((num, index) => {
+              return (
+                <div>
+                  <div>{`${index + 1} 组`}</div>
+                  <div>
+                    {num.map(() => {
+                      return <div className="item">{`${students.id} ${students.name}`}</div>;
+                    })}
+                  </div>
+                </div>
+              );
+            })}
         </div>
         <div className="student-list">
           <h3>学员列表</h3>
